7669719063bd3dd94166e87b1bb86a3ba62bdeb2
Safiyya Sharif <SafiyyaSharif@Safiyyas-MacBook-Air.local>
2017-07-16 16:04:36 -0400

    Initial Commit

---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'wanYBTjWMSCq3opgiojvHmJfk',
+  consumer_secret: 'UeCjAOlFd99Y0wAMUGtu7wKyAlqUSDj4kxDpyHfoCe6dDWiBZR',
+  access_token_key: '886669466158772224-urX9vseuSJCoJbnkvUGkp7RGxQIgNba',
+  access_token_secret: 'HBTVWgnIh95nSP1tbeAzxq92njcWFVDJsqE9xEtNDZE25',
+}
+exports.spotifyKeys = {
+  id: 'b6753f8bad174308b7736004eb6b14ae',
+  secret: '56cc4969b926492d91488a871982b0f1',
+}
+exports.omdbKeys = {
+  API_key: '40e9cece',
+
+}
